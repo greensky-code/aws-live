0d313b4e8a44d5c7da6ade31ba223c9d48db1575
Ameer Almutairi <c0049693@newcastle.ac.uk>
2021-08-15 05:06:43 +0530

    push cod

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-2.cgvaf4jqhktj.us-east-2.rds.amazonaws.com"
+customuser = "admin"
+custompass = "testpassword"
+customdb = "employee"
+custombucket = "s3employee"
+customregion = "us-east-2"
